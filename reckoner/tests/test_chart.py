560b9ad8e4996b52bedef5d0ac60e8c3e2ffec66
Nick Huanca <nick@reactiveops.com>
2019-02-12 00:12:55 -0700

    Adjustments
    Fixed the logging behavior of hook events: Now hooks will alwyas show the output they have (stderr and/or stdout).
    
    * Fixed bug with incorrect impl for python2 for truthy class objects
    * Added tests to assure we're logging errors on failed exit codes
    * Added tests to assure we're catching uncaught exceptions so click can handle them

---
+++ reckoner/tests/test_chart.py
@@ -0,0 +1,61 @@
+"""Test the chart functions directly"""
+import unittest
+import mock
+from reckoner.chart import Chart
+from reckoner.command_line_caller import Response
+from reckoner.exception import ReckonerCommandException
+
+
+@mock.patch('reckoner.chart.Repository')
+@mock.patch('reckoner.chart.Config')
+# I have to strictly design the mock for Reckoner due to the nature of the
+# key/val class setup. If the class actually had attributes then this
+# would be more easily mockable
+@mock.patch('reckoner.chart.call')
+class TestChartHooks(unittest.TestCase):
+    def setUp(self):
+        self._chart = Chart(
+            {'name': {
+                'hooks': {
+                    'pre_install': [
+                        'omg',
+                    ],
+                    'post_install': [
+                        'fetchez --la-vache',
+                        'mooooooooo!',
+                    ]
+                }
+            }
+            },
+            None,
+        )
+
+    def test_caught_exceptions(self, mock_cmd_call, *args):
+        """Assert that we raise the correct error if call() blows up"""
+        mock_cmd_call.side_effect = [Exception('holy smokes, an error!')]
+        with self.assertRaises(ReckonerCommandException):
+            self._chart.run_hook('pre_install')
+
+    @mock.patch('reckoner.chart.logging')
+    def test_logging_info_and_errors(self, mock_logging, mock_cmd_call, *args):
+        """Verify we log the right info when call() fails and succeeds"""
+        bad_response = mock.Mock(Response,
+                                 exitcode=1,
+                                 stderr='some error',
+                                 stdout='some info',
+                                 command_string='my --command')
+        good_response = mock.Mock(Response,
+                                  exitcode=0,
+                                  stderr='',
+                                  stdout='some info',
+                                  command_string='my --command')
+        mock_cmd_call.side_effect = [good_response]
+        self._chart.run_hook('pre_install')
+        self.assertEqual(mock_logging.error.call_count, 0)
+        self.assertTrue(mock_logging.info.call_count > 0)
+
+        mock_cmd_call.side_effect = [bad_response, good_response]
+        mock_logging.reset_mock()
+        self._chart.run_hook('post_install')
+        self.assertTrue(mock_logging.error.call_count > 0)
+        self.assertTrue(mock_logging.info.call_count > 0)
