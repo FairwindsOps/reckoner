7d8dd9b2d814344c845668f0121af98167b63b2c
Nick Huanca <nick@reactiveops.com>
2019-02-18 08:54:41 -0700

    Rearranged a test for better actionability on failures.

--- reckoner/tests/test_config.py
+++ reckoner/tests/test_config.py
@@ -6,11 +6,11 @@ class TestConfig(unittest.TestCase):
     def test_course_base_dir_never_empty(self):
         config = Config()
         config.course_path = 'course.yaml'
-        self.assertIsNone(config.course_base_directory)
         self.assertNotEqual('', config.course_base_directory,
                             "course_base_path has to be None or a real path "
                             "(including '.') because of how it is used in "
-                            "Popen.")
+                            "Popen. Cannot be an empty string.")
+        self.assertIsNone(config.course_base_directory)
 
         config.course_path = '/some/full/path/course.yml'
         self.assertEqual('/some/full/path', config.course_base_directory)
