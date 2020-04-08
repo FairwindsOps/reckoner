a00f1ad4e8d1ba8439e3835b5af1ced55cd14946
bambash <bambash256@gmail.com>
2020-04-08 15:00:24 -0500

    updated setup_helm3.sh

--- end_to_end_testing/setup_helm3.sh
+++ end_to_end_testing/setup_helm3.sh
@@ -1,6 +1,6 @@
 source "$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/setup_common.sh
 
-echo "Installing Helm"
+echo "Installing Helm 3.1.2"
 curl -sL https://get.helm.sh/helm-v3.1.2-linux-amd64.tar.gz | tar xzv linux-amd64/helm
 sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -rf linux-amd64
