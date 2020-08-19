e590fa643a7862a33000b5aa4eeac2bf495f9ac5
Andrew Suderman <andrew@sudermanjr.com>
2020-08-19 10:59:40 -0600

    Update helm to 3.3.0 for testing and validation (#218)

--- end_to_end_testing/setup_helm3.sh
+++ end_to_end_testing/setup_helm3.sh
@@ -1,7 +1,7 @@
 source "$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/setup_common.sh
 
-echo "Installing Helm 3.1.2"
-curl -sL https://get.helm.sh/helm-v3.1.2-linux-amd64.tar.gz | tar xzv linux-amd64/helm
+echo "Installing Helm 3.3.0"
+curl -sL https://get.helm.sh/helm-v3.3.0-linux-amd64.tar.gz | tar xzv linux-amd64/helm
 sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -rf linux-amd64
 helm version
