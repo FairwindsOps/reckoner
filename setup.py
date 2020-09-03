1516e168947037a9b297a884d12a6b2d8b7621b2
Andrew Suderman <andrew@sudermanjr.com>
2020-09-03 15:53:19 -0600

    doh

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except ImportError:
 
 
 setup(name='reckoner',
-      python_requires='~=3.8',
+      python_requires='~=3.6',
       use_scm_version=True,
       setup_requires=['setuptools_scm'],
       description='Declarative Helm configuration with Git capability',
