6e18ab812ce25a886d642b6abe9cc7a783376c21
EJ Etherington <eetherington@peak6.com>
2020-12-10 18:33:15 -0800

    trying internet workaround for ruamel thingie https://github.com/opencobra/cobrapy/pull/894/files

--- reckoner/yaml/handler.py
+++ reckoner/yaml/handler.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ruamel.yaml import YAML
+from ruamel.yaml.main import YAML
 from ruamel.yaml.constructor import DuplicateKeyError
 from reckoner.exception import ReckonerConfigException
 import logging
