06cad43cc2c999523cfe07b5e8a29f326fc87e9a
Andrew Suderman <andrew@sudermanjr.com>
2020-12-16 15:50:17 -0500

    remove helm 2 client (#256)

--- end_to_end_testing/run.sh
+++ end_to_end_testing/run.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
 set -x
+set -e
+
+python --version
+
 chmod +x /usr/local/bin/reckoner
 reckoner version
-
-## Install python 3.8 and pip
-#apt update
-#apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-#wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
-#tar xzf Python-3.8.5.tgz
-#cd Python-3.8.5
-#./configure --enable-optimizations
-#make altinstall
-#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#python3.8 get-pip.py
-## Install reckone rwith python3.8
-#pip3.8 install /reckoner
 
 curl -LO https://github.com/ovh/venom/releases/download/v0.27.0/venom.linux-amd64
 mv venom.linux-amd64 /usr/local/bin/venom
