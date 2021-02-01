edc27b8c5680911265f80776e1dd9368e8118f43
ejether <eetherington@peak6.com>
2021-02-01 10:20:30 -0800

    Enhanced Secret Functionality (#271)
    * Adding the schema validation and tests for the secrets heading
    
    * adding secrets parsing to course and tests for Secret() attributes
    
    * adding SecretProvider base class
    
    * connecting the dots. setting up the merge of the secrets into the os.environ dictionary before any chart commands are run.
    
    * added shell executor type secret backend for those who want more functionality but don't want to write python
    
    * adding more testing of providers
    
    * improving coverage of new code
    
    * adding docs
    
    * Apply suggestions from code review
    
    Adding missing newlines
    
    * Apply suggestions from code review
    
    I swear I did use a spell check....
    
    Co-authored-by: Andrew Suderman <andrew@sudermanjr.com>
    
    * adding shell executor e2e test
    
    Co-authored-by: Andrew Suderman <andrew@sudermanjr.com>

---
+++ reckoner/secrets/providers/base.py
@@ -0,0 +1,27 @@
+# Copyright 2019 FairwindsOps Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC, abstractmethod
+
+
+class SecretProvider(ABC):
+    """
+    Abstract base class for secret providers to inherit
+    from to ensure SecretProvider.get_value() method
+
+    When writing new SecretProviders, always inherit from this class.
+    """
+    @abstractmethod
+    def get_value(self):
+        pass
