f2907bb08e70e2f12921947ef392a28bb1185e86
Adam Z <69020096+Azahorscak@users.noreply.github.com>
2021-02-15 16:30:37 -0500

    Validation fixes (#307)
    * fix validation
    
    * validation tests
    
    * remove comments
    
    Co-authored-by: Andrew Suderman <andrew@sudermanjr.com>

--- pkg/reckoner/validation.go
+++ pkg/reckoner/validation.go
@@ -5,9 +5,14 @@ import (
 	"os"
 )
 
+// ValidateArgs validates that the correct arguments were passed and returns the name of the course file
 func ValidateArgs(runAll bool, onlyRun, args []string) (string, error) {
 	if runAll && len(onlyRun) != 0 {
 		return "", fmt.Errorf("you must either use run-all or only")
+	}
+
+	if !runAll && len(onlyRun) == 0 {
+		return "", fmt.Errorf("you must use at least one of run-all or only")
 	}
 
 	if len(args) != 1 {
@@ -18,5 +23,5 @@ func ValidateArgs(runAll bool, onlyRun, args []string) (string, error) {
 	if os.IsNotExist(err) {
 		return "", fmt.Errorf("specified course file %s does not exist", args[0])
 	}
-	return args[0], nil
+	return args[0], err
 }
