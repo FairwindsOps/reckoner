717fc28bb7dc67315370653594cf03a5652c1bf5
Luke Reed <luke@lreed.net>
2021-09-14 16:58:00 -0400

    change to basic-demo chart instead of ingress-nginx

--- end_to_end_testing/run_python.sh
+++ end_to_end_testing/run_python.sh
@@ -21,5 +21,5 @@ mkdir -p /tmp/test-results
 cd /reckoner/end_to_end_testing
 
 # The parallelization number must remain relatively low otherwise the tests become flaky due to resources and pending pods and such
-venom run tests/* --log debug --output-dir=/tmp/test-results --strict
+venom run tests/* --log debug --output-dir=/tmp/test-results --parallel=2 --strict
 exit $?
