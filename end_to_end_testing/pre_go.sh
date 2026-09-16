008e4b46c89e5dd31a223e2d32192039a3cf24ff
jdesouza <james@fairwinds.com>
2026-09-16 17:17:58 -0300

    Fix scripts

--- end_to_end_testing/pre_go.sh
+++ end_to_end_testing/pre_go.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
 
+make build
+docker exec e2e-command-runner mkdir -p /tmp/test-results
 docker cp . e2e-command-runner:/reckoner
-docker cp /dist e2e-command-runner:/reckoner
--- end_to_end_testing/run_go.sh
+++ end_to_end_testing/run_go.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-
-set -x
 set -e
 
-# Install Go
-curl -LO https://go.dev/dl/go1.20.7.linux-amd64.tar.gz
-
-tar -C /usr/local -xzf go1.20.7.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
-
-# build
-cd /reckoner
-make build
-
-mv /reckoner/reckoner /usr/local/bin/reckoner
+cp /reckoner/reckoner /usr/local/bin/reckoner
+chmod +x /usr/local/bin/reckoner
 reckoner version
 
 curl -LO https://github.com/ovh/venom/releases/download/v1.1.0/venom.linux-amd64
@@ -27,4 +15,3 @@ cd /reckoner/end_to_end_testing
 
 # The parallelization number must remain relatively low otherwise the tests become flaky due to resources and pending pods and such
 venom run tests/* --output-dir=/tmp/test-results
-exit $?
